b66959242e3f29eeb732b522dc5a715a8fba39a3
Fra-D-L <francesco.delellis@edu.unito.it>
2025-06-17 18:09:49 +0200

    Aggiunta formula per z-test

---
+++ z-test_formula.R
@@ -0,0 +1,17 @@
+z.test <- function(x, sigma, mu = 0) {
+  n <- length(x)
+  xbar <- mean(x)
+  Z <- (xbar - mu) / (sigma / sqrt(n))
+  p_val <- 2 * (1 - pnorm(abs(Z)))
+  if (n < 30) {
+    cat("Warning!\nA sample size of ", n, " is too small to perform an accurate Z test (n is < 30)\n")
+  }
+  cat("Z-test\nZ = ", Z, "\tp-value =", p_val, "\n")
+}
+
+z.test(
+  c(105, 105, 105, 107, 103, 110, 100, 205, 5, 105, 105, 105, 105, 105, 105, 105, 105, 105, 105, 105, 105,
+    105, 105, 105, 105, 105, 115, 95, 105),
+  sigma=15,
+  mu=100
+)
